13aebf5b01e4e11c2aa0a8a708b66d51c0dd029f
gyoerkaa <gyoerkaa@outlook.com>
2013-02-07 22:32:01 +0100

    Initial commit

---
+++ dummyfolder/dummyprogram.bat
@@ -0,0 +1,14 @@
+:: This does nothing, but display the changed variables
+@ECHO off
+
+@ECHO.
+@ECHO Hello, I'm the dummy program
+@ECHO I'm the best at what I do and what I do is printing path variables !
+@ECHO.
+@ECHO %USERPROFILE%
+@ECHO %HOMEPATH%
+@ECHO %HOMEDRIVE%
+@ECHO %APPDATA%
+@ECHO %ALLUSERSPROFILE%
+@ECHO %PROGRAMDATA%
+@ECHO %PUBLIC%
